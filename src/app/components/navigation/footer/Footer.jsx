8054e1f2fc31a642f1e15c47eb0fabad4a25a0e2
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-11-27 18:29:11 +0000

    background added

--- src/app/components/navigation/footer/Footer.jsx
+++ src/app/components/navigation/footer/Footer.jsx
@@ -14,20 +14,20 @@ export default function Footer() {
     },
   ];
   return (
-    <footer className="rounded-lg bg-[#256EFF] m-4 drop-shadow-lg">
+    <footer className="rounded-lg bg-[#1e217a] m-4 drop-shadow-lg">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a
             href="/"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
-            <span className="self-center text-2xl whitespace-nowrap text-white font-thin">
+            <span className="self-center text-2xl whitespace-nowrap">
               Jason Blackburn
             </span>
           </a>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0">
             {footerLinks.map(({ link, title }) => (
-              <li className="font-thin text-xl">
+              <li className="text-xl">
                 <a href={link} class="hover:underline me-4 md:me-6">
                   {title}
                 </a>
@@ -36,7 +36,7 @@ export default function Footer() {
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-white sm:text-center">
+        <span className="block text-sm sm:text-center">
           © 2023{" "}
           <a href="https://flowbite.com/" className="hover:underline">
             Jason Blackburn™
--- src/app/components/navigation/navbar/Navbar.jsx
+++ src/app/components/navigation/navbar/Navbar.jsx
@@ -25,11 +25,11 @@ export default function Navbar() {
   const [nav, setNav] = useState(false);
 
   return (
-    <nav className="h-16 text-white flex justify-between items-center m-4 rounded-lg drop-shadow-lg">
-      <h1 className="ml-4 font-thin text-2xl">Jason Blackburn</h1>
-      <ul className="hidden md:flex font-thin">
+    <nav className="h-16 flex justify-between items-center m-4 rounded-lg drop-shadow-lg">
+      <h1 className="ml-4 text-2xl">Jason Blackburn</h1>
+      <ul className="hidden md:flex">
         {links.map(({ link, title, id }) => (
-          <li key={id} className="mx-4 text-xl">
+          <li key={id} className="mx-4 text-xl hover:underline">
             <Link href={link}>{title}</Link>
           </li>
         ))}
@@ -43,12 +43,9 @@ export default function Navbar() {
       </div>
 
       {nav && (
-        <ul className="absolute top-20 left-0 z-30 flex flex-col items-center justify-center w-full h-50 text-white bg-[#256EFF] rounded-lg animate-flip-down">
+        <ul className="absolute top-20 left-0 z-30 flex flex-col items-center justify-center w-full h-50 bg-[#1e217a] rounded-lg animate-flip-down">
           {links.map(({ id, title, link, att }) => (
-            <li
-              key={id}
-              className="my-4 text-xl font-thin capitalize hover:bg-[#3DDC97]"
-            >
+            <li key={id} className="my-4 text-xl capitalize hover:bg-[#3DDC97]">
               <Link onClick={() => setNav(!nav)} target={att} href={link}>
                 {title}
               </Link>
